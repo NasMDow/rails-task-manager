7f1fca36fa6dab1202d3040626f4d3c809e4c9a2
Nasreen Dowlut <nasmdow@gmail.com>
2021-07-27 13:46:05 +0100

    tasks challenge complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,48 @@
+#Tasks Controller
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    # Ici on met la logique pour créer le restaurant
+    @task = Task.new(task_params)
+    @task.save
+    flash[:notice] = 'A new task was added'
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    @task.save
+    flash[:notice] = 'Task was updated'
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    flash[:notice] = 'Task was deleted'
+    redirect_to task_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- db/migrate/20210727084848_create_tasks.rb
+++ db/migrate/20210727084848_create_tasks.rb
@@ -1,10 +1,11 @@
+# CreateTasks
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.string :title
       t.text :details
-      t.false :completed
-
+      t.boolean :completed, default: false
+ 
       t.timestamps
     end
   end
